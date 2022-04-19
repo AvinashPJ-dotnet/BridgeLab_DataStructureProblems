57b3d06f393b3dd8c150c214345ecfa7099a9122
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-19 19:10:55 +0530

    Added: node, interface and my linked list class and methods

---
+++ src/test/java/MyLinkedListTest.java
@@ -0,0 +1,21 @@
+
+import org.junit.jupiter.api.Assertions;
+import un_ordered_list.MyLinkedList;
+import un_ordered_list.MyNode;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedListTest {
+    @Test
+    public void givenArrayOfWords_WhenAddedAndSearch_ShouldMatchPosition() {
+        String[] words = {"hi", "this", "is", "the", "test", "case"};
+        MyLinkedList myLinkedList = new MyLinkedList();
+        for (int i = 0; i < words.length; i++) {
+            MyNode<Object> newWord = new MyNode<>(words[i]);
+            myLinkedList.append(newWord);
+        }
+        myLinkedList.printMyNode();
+        MyNode<Object> searchNode = new MyNode<>("case");
+        int pos = myLinkedList.search(searchNode);
+        Assertions.assertEquals(5,pos);
+    }
+}
